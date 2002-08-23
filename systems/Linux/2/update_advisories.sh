e73baa751db8f815e1814b5b9c41ada44c6e108f
Javier Fernandez-Sanguino <jfs@debian.org>
2002-08-23 13:21:50 +0000

    New check for Linux. This one uses netstat and lsof (the previous one in scripts/ did this but I removed netstat since this script uses Linux specific options)

---
+++ systems/Linux/2/update_advisories.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Updates the information distributed by Tiger with the latest security
+# advisories. Currently only works with Debian (DSAs) which are parsed
+# from the WML sources (retrieved from CVS at DEB_ADV_DIR)
+#
+#     Copyright (C) 2002 Javier Fernandez-Sanguino
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 1, or (at your option)
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#     Please see the file `COPYING' for the complete copyright notice.
+
+
+DEB_ADV_DIR=~jfs/debian/www/webml/english/security/
+if [ -d $DEB_ADV_DIR ] 
+then
+	find $DEB_ADV_DIR -name "*data" -exec cat \{\} \; | perl retrieve-advisories.pl >debian_advisories
+else
+	echo "Cannot access the $DEB_ADV_DIR directory."
+	echo "Cannot generate the advisories data for Tiger."
+	exit 1
+fi
+
+exit 0
+
+
